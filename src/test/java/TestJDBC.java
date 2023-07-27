82eb8d0d8ce9fc914660bbc5832879ab64e2a79d
Asus <tetiana.kostyhina@gmail.com>
2023-07-27 18:40:11 +0300

    HW_20(2)

---
+++ src/test/java/TestJDBC.java
@@ -0,0 +1,28 @@
+import objects.User;
+import org.junit.Assert;
+import org.junit.Test;
+import selection.Students;
+
+import java.sql.SQLException;
+
+public class TestJDBC {
+
+    @Test
+    public void testAddStud() throws SQLException {
+        Assert.assertFalse(Students.addUser("Anna", "Moris", "Dnipro"));
+    }
+
+    @Test
+    public void testGetDateByName() {
+        for (User l : Students.getUserObjects("Tetiana")) {
+            System.out.println(l.date);
+        }
+
+
+    }
+
+
+    }
+
+
+
